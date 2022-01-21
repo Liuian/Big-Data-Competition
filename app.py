d944887c129fad5df8c87d548f9e964f8916ff24
F64081169 <62001405+F64081169@users.noreply.github.com>
2022-01-21 16:07:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,29 +6,45 @@ from dotenv import load_dotenv
 from linebot import LineBotApi, WebhookParser
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from apscheduler.schedulers.background import BlockingScheduler
+from apscheduler.triggers.date import DateTrigger
+
+# 連結google sheet
+#import gspread
+#from oauth2client.service_account import ServiceAccountCredentials
+'''
+import sys
+import datetime
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials as SAC
+'''
+#--------
 
 from fsm import TocMachine
 from utils import send_text_message
-
+import schedule
+import time
 load_dotenv()
 
-
+notify = 1
 machine = TocMachine(
-    states=["user", "state1", "state2"],
+    states=["user", "enterFood","enterDate","enternum", "comfirm", "showAll","deletedfood", "delete","recommand"],
     transitions=[
-        {
-            "trigger": "advance",
-            "source": "user",
-            "dest": "state1",
-            "conditions": "is_going_to_state1",
-        },
-        {
-            "trigger": "advance",
-            "source": "user",
-            "dest": "state2",
-            "conditions": "is_going_to_state2",
-        },
-        {"trigger": "go_back", "source": ["state1", "state2"], "dest": "user"},
+        #輸入食材流程
+        {"trigger": "advance","source": "user","dest": "enterFood","conditions": "is_going_to_enterFood",},
+        {"trigger": "advance","source": "enterFood","dest": "enterDate","conditions": "is_going_to_enterDate",},
+        {"trigger": "advance","source": "enterDate","dest": "enternum","conditions": "is_going_to_enternum",},
+        {"trigger": "advance","source": "enternum","dest": "comfirm","conditions": "is_going_to_comfirm",},
+        
+        #顯示冰箱的菜
+        {"trigger": "advance","source": "user","dest": "showAll","conditions": "is_going_to_showAll",},
+        #刪除食材
+        {"trigger": "advance","source": "user","dest": "deletedfood","conditions": "is_going_to_deletedfood",},
+        {"trigger": "advance","source": "deletedfood","dest": "delete","conditions": "is_going_to_delete",},
+        #推薦食譜一
+        {"trigger": "advance","source": "user","dest": "recommand","conditions": "is_going_to_recommand",},
+
+        {"trigger": "go_back", "source": ["enterFood", "showAll","enterDate","comfirm","delete","recommand"], "dest": "user"},
     ],
     initial="user",
     auto_transitions=False,
@@ -37,6 +53,9 @@ machine = TocMachine(
 
 app = Flask(__name__, static_url_path="")
 
+def push_message(push_text_str):
+    line_bot_api.push_message(user_id, TextSendMessage(text=push_text_str))
+    
 
 # get channel_secret and channel_access_token from your environment variable
 channel_secret = os.getenv("LINE_CHANNEL_SECRET", None)
@@ -50,6 +69,8 @@ if channel_access_token is None:
 
 line_bot_api = LineBotApi(channel_access_token)
 parser = WebhookParser(channel_secret)
+user_id = "U227736503c290a9f5fbe50b3423d5df2"
+
 
 
 @app.route("/callback", methods=["POST"])
@@ -81,17 +102,33 @@ def callback():
 
 @app.route("/webhook", methods=["POST"])
 def webhook_handler():
+    # test connect to google sheet
+    '''
+    def gsheet(self, stocks):
+        scopes = ["https://spreadsheets.google.com/feeds"]
+ 
+        credentials = ServiceAccountCredentials.from_json_keyfile_name(
+	    "credentials.json", scopes)
+ 
+        client = gspread.authorize(credentials)
+ 
+        sheet = client.open_by_key(
+	        "b86448485f36973bf9e1af9dc6d52291c79338b6").sheet1  #第一個工作表
+    # ---------------
+    '''
     signature = request.headers["X-Line-Signature"]
+    notify = 1
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info(f"Request body: {body}")
+    
 
     # parse webhook body
     try:
         events = parser.parse(body, signature)
     except InvalidSignatureError:
         abort(400)
-
+    
     # if event is MessageEvent and message is TextMessage, then echo text
     for event in events:
         if not isinstance(event, MessageEvent):
@@ -102,10 +139,15 @@ def webhook_handler():
             continue
         print(f"\nFSM STATE: {machine.state}")
         print(f"REQUEST BODY: \n{body}")
+        
         response = machine.advance(event)
         if response == False:
             send_text_message(event.reply_token, "Not Entering any State")
-
+        if notify == 1:
+            #push_message("for testing")
+            notify = 0
+        
+    
     return "OK"
 
 
@@ -117,4 +159,6 @@ def show_fsm():
 
 if __name__ == "__main__":
     port = os.environ.get("PORT", 8000)
+    
     app.run(host="0.0.0.0", port=port, debug=True)
+
